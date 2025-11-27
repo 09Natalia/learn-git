2cb557ab70f07a70b20fc267158a2e8960465737
Natalia Eroshkina <09natasha.e@gmail.com>
2025-11-27 22:39:39 +0700

    new test with search

--- test_bbc.cy.js
+++ test_bbc.cy.js
@@ -3,36 +3,22 @@
 describe('Bbc navigation', () => {
   beforeEach(() => {
     cy.visit('https://www.bbc.com/');
-    cy.wait(2000);
+    cy.wait(1000);
   });
 
 
   it('Открытие главной страницы и проверка заголовка', () => {
 
     // Проверка заголовка главной страницы
-    cy.title().should('eq', 'BBC Home - Breaking News, World News, US News, Sports, Business, Innovation, Climate, Culture, Travel, Video & Audio')
+    cy.title({ timeout: 10000 }).should('include', 'BBC Home')
 
   })
 
   it('Есть кнопки логин и регистрация', () => {
-    cy.get('.sc-8342e32-8.jLoJFd') // проверяем наличие кнопки и текста на ней
-      .should('exist')
-      .contains('Register')
-  
-
-    //cy.get('.sc-8342e32-8.jLoJFd') // проверяем ссылку в кнопке
-   // .closest('a') // ищем ссылку, обернутую вокруг кнопки
-    //.and('have.attr', 'href')
-   // .and('include', '/register')
-  
-    cy.get('.sc-28fdc07f-2.sc-28fdc07f-5.jumpKn.dSGHyC').should('exist')
-      .should('exist')
-      .contains('Sign In')
-
     //тесты те же, только другой вариант
     cy.get('button[aria-label="Register"]', { timeout: 10000 }).should('be.visible') // ок
     cy.get('button[aria-label="Sign In"]', { timeout: 10000 }).should('be.visible') // ок
-    cy.wait(2000);
+    cy.wait(1000);
   })
 
   it('Проверка наличия логотипа', () => {
@@ -40,7 +26,7 @@ describe('Bbc navigation', () => {
       .should('have.length', 2)
       .and('be.visible')
 
-    cy.wait(2000);  
+    cy.wait(1000);  
   })
 
   it('Проверка иконок соц.сетей', () => {
@@ -50,7 +36,7 @@ describe('Bbc navigation', () => {
    cy.get('button[aria-label="Follow BBC on tiktok"]').should('exist')
    cy.get('button[aria-label="Follow BBC on linkedin"]').should('exist')
    cy.get('button[aria-label="Follow BBC on youtube"]').should('exist')
-   cy.wait(2000);
+   cy.wait(500);
   
   })
 
@@ -65,33 +51,123 @@ describe('Bbc navigation', () => {
       .should('have.attr', 'href')  
       .and('include', '/news')
 
-    cy.wait(2000);
+    cy.wait(1000);
   })
 
-    it('Переход на News', () => {
-    
-    //cy.contains('News').click() // не открывает
-
+  it('Переход на News', () => {
     cy.get('footer').contains('News').should('be.visible') // новости есть в фетере
     cy.get('footer').contains('News').click() // клик
 
-    cy.url().should('include', '/news')
-    cy.title().should('eq', 'BBC News - Breaking news, video and the latest top stories from the U.S. and around the world')
-    cy.wait(2000);
+    cy.url({ timeout: 10000 }).should('include', '/news')
+    cy.title({ timeout: 10000 }).should('include', 'BBC News')
+    cy.wait(1000);
   })
 
+  it('Клик "назад" с вкладки News', () => {
+    cy.visit('https://www.bbc.com/');
+    cy.get('footer').contains('News').should('be.visible') // новости есть в фетере
+    cy.get('footer').contains('News').click() // клик
+    cy.go('back') // кликаем "назад"
+    cy.url({ timeout: 10000 }).should('not.include', '/news')
+    cy.title().should('include', 'BBC Home')
+    cy.wait(1000);
+  })
+
+    it('Переход вкладку Asia с вкладки News', () => {
+    cy.viewport(1280, 800);
+    cy.visit('https://www.bbc.com/news');
+    cy.get('nav').should('contain.text', 'Asia');
+    cy.contains('[data-testid="subNavigationLink"]', 'Asia').click();
+    cy.url().then(u => cy.log('URL now:', u));
+    cy.url({ timeout: 10000 }).should('include','/news/world/asia');
+    
+    cy.wait(1000);
+  })
+
+  it('Скролл до Features на странице Asia и клик по первой статье', () => {
+    cy.viewport(1280, 800);
+    cy.visit('https://www.bbc.com/news/world/asia');
+    cy.contains('[data-testid="section-title-wrapper"]','Features') // скролл до блока
+      .scrollIntoView()
+      .should('be.visible');
+    //надо найти контейнер карточек и выбрать 1ю
+    cy.get('[data-testid="nevada-section-5"]')
+      .find('[data-testid="nevada-grid-5"]')
+       .eq(1)
+      .scrollIntoView()
+      .within(() => { //функция внутри родит эл-та
+          cy.get('[data-testid="anchor-inner-wrapper"]').first().click();
+        });
+    cy.wait(500);    
+  
+  })
+
+})
+
+describe('Bbc search', () => {
+  beforeEach(() => {
+    cy.visit('https://www.bbc.com/');
+    cy.wait(500);
+  });
 
   it('Поиск через search по слову Canada', () => {
     
     cy.get('button[aria-label="Open menu"]').click({ force: true })  // кликаем по поиску
     cy.get('input[placeholder="Search news, topics and more"]').type('Canada{enter}') // ввод в строку поиска Canada
-    cy.url().should('include', '/search') // поиск выполнен, в урл есть /search
+    //cy.url().should('include', '/search?q=Сanada') // поиск выполнен, в урл есть /search?q=canada
+    cy.url({ timeout: 10000 }).should('match', /\/search\?q=canada/i)
     cy.contains('Canada', { matchCase: false }).should('exist')
-    cy.wait(2000);
+    cy.wait(500);
+  } )
+
+  it('Есть результаты после обновления страницы', () => {
+    
+    cy.get('button[aria-label="Open menu"]').click({ force: true }) 
+    cy.get('input[placeholder="Search news, topics and more"]').type('Canada{enter}') 
+    //cy.url().should('include', '/search?q=Сanada')
+    cy.url().should('match', /\/search\?q=canada/i)
+    cy.contains('Canada', { matchCase: false }).should('exist')
+
+    cy.reload(); //обновляем стр
+
+    cy.url({ timeout: 10000 }).should('include', '/search'); // или проверка query param, если есть
+    cy.contains('Canada', { matchCase: false }).should('exist');
+    cy.get('[data-testid="new-jersey-grid"]').should('exist').and('not.be.empty');
+
+
+    cy.wait(500);
   } )
 
 
-})
 
 
+  it('Тест на поиск по слову с пробелами и на чувствительность к регистру', () => {
+    cy.get('button[aria-label="Open menu"]').click({ force: true });
+    cy.get('input[placeholder="Search news, topics and more"]')
+      .type(' canada {enter}'); // вставляем пробемы до и после слова
+    // Проверяем, что URL содержит параметр q (регулярка, чтобы учесть кейс)
+    //cy.url().should('match', /\/search\?q=canada/i);
+    //cy.url().should('include', '/q=сanada') 
+    cy.url({ timeout: 10000 }).should('match', /\/search\?q=+canada+/i)
+
+    // Проверка на регистр
+    cy.contains('CaNAada', { matchCase: false }).should('exist'); // игнорируем регистр
+    cy.get('[data-testid="new-jersey-grid"]')  // есть блок с результатами
+      .should('exist')
+      .and('not.be.empty');
+  });
+
+
+
+
+
+
+
+  
+})  
+
+
+
+
+    
 
